b43b740b6bbc5cf8812fec7a4ee1016ce97224d1
zkjaisq <18771964684@163.com>
2019-08-15 11:46:32 +0800

    添加webpack externals配置

--- lib/button.tsx
+++ lib/button.tsx
@@ -1,7 +1,9 @@
+// 为什么要引入react的原因，下面的div是一个语法糖，和上面的注释的语法相同。
+import React from 'react'
 function Button(){
     return(
-        <div>button1</div>
+    //  React.createElement('div',null,'button1')
+    <div>button2</div>
     )
 }
-
 export default Button;
--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,20 @@ module.exports= {
         new HtmlWebpackPlugin({
             template: 'index.html'
         })
-    ]
-
+    ],
+    // 防止将某些import的包打包到bundle中，而是在运行的时候再去外部获取这些扩展依赖。
+    externals:{
+        react: {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react',
+            root: 'React',//使用script标签引入的
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+            root: 'ReactDOM',
+        },
+    }
 }
