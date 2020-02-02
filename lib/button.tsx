e4bdbb25d86acb786228738af057aec7bfb39f6c
zkjaisq <18771964684@163.com>
2020-02-02 12:16:07 +0800

    add ignore

--- lib/button.tsx
+++ lib/button.tsx
@@ -1,5 +1,5 @@
 // 为什么要引入react的原因，下面的div是一个语法糖，和上面的注释的语法相同。
-import React from 'react'
+import * as React from 'react'
 function Button(){
     return(
     //  React.createElement('div',null,'button1')
