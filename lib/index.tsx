a0a29830448fa5b490de35361b3cf4ad0bb5ea48
zkjaisq <18771964684@163.com>
2020-02-05 17:16:58 +0800

    添加svgloader并配置webpack.config和ts.config

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,8 @@
 import Icon from './icon';
 import * as React from 'react'
 import * as ReactDOM from 'react-dom';
+import wechat from './wechat.svg'
+
+console.log(wechat);
 
 ReactDOM.render(<Icon name='wechat'/>,document.body.querySelector('#root'))
