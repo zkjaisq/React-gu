95025bc666f84285a903359d7c76dc7b7a6aa32c
zkjaisq <18771964684@163.com>
2020-02-07 20:29:06 +0800

    给icon添加事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import './importIcons';
 import './icon.scss'
-interface IconProps {
+interface IconProps extends React.SVGAttributes<SVGElement>{
     name:string;
 }
+
 const Icon:React.FunctionComponent<IconProps> = (props)=> {
-    return(
-        <svg className="icon">
+    
+    return (
+        <svg className="icon" {...props}>
             <use xlinkHref={`#${props.name}`}/>
         </svg>
     )
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,12 +2,14 @@ import Icon from './icon';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-
+const  fn :React.MouseEventHandler = (e)=> {
+    console.log(e.target);
+}
 
 ReactDOM.render(
     <div>
-        <Icon name='wechat'/>
-        <Icon name='ali'/>
-        <Icon name='qq'/>
+        <Icon name='wechat' onClick={fn}/>
+        <Icon name='ali' onClick={fn}/>
+        <Icon name='qq' onClick={fn}/>
     </div>
    ,document.body.querySelector('#root'))
