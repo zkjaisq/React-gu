00c322df6dc734f684ac7589513bcc96800094b9
zkjaisq <18771964684@163.com>
2020-02-09 11:15:55 +0800

    添加测试覆盖率

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,16 @@
 module.exports = {
   verbose: true,
   clearMocks: false,
-  collectCoverage: false,
-  reporters: ["default"],
+  //是否收集test的
+  collectCoverage: true,
+  //jest-junit  是一个测试的分析报告 。
+  reporters: ["default","jest-junit"],
+
+  //测试覆盖率的配置
+  collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleDirectories: ['node_modules'],
   globals: {
@@ -14,6 +22,7 @@ module.exports = {
   },
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
+    "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js",
   },
   testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
   transform: {
