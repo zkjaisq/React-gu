ac4c95fed5a85d394d53f7679185d93d65ef0305
zkjaisq <18771964684@163.com>
2020-02-10 12:09:29 +0800

    制作简单的demo

---
+++ example.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDom from  'react-dom'
+import iconexample from './lib/icon/icon.example'
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+
+ReactDom.render((
+  <Router>
+      <div>
+          <header className="logo">logo</header>
+          <div>
+              <aside>
+                  <ul>
+                      <li>
+                          <Link to="/icon">icon</Link>
+                      </li>
+                      <li>
+                          <Link to="/button">button</Link>
+                      </li>
+                  </ul>
+              </aside>
+              <main>
+                  <Switch>
+                     <Route path="/icon" component={iconexample}></Route>
+                  </Switch>
+              </main>
+          </div>
+      </div>
+  </Router>
+
+),document.querySelector('#root'))
---
+++ lib/icon/icon.example.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Icon from './icon'
+const iconexample:React.FunctionComponent = ()=>{
+    return (
+        <div>
+            <Icon name="ali"/>
+            <Icon name="wechat"/>
+            <Icon name="qq"/>
+        </div>
+    )
+}
+
+export default iconexample
